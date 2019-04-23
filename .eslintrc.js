ca86dda028a895d6ed5b666e3164f5e7f0ab1f1c
Chris McNeese <christophermcneese@mac.com>
2019-04-23 14:35:59 -0500

    removed old script from linter

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,31 +9,5 @@ module.exports = {
     browser: true
   },
   rules: {
-<<<<<<< HEAD
   }
-=======
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js'
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
-      },
-      env: {
-        browser: false,
-        node: true
-      }
-    }
-  ]
->>>>>>> 9b5b767... message
 };
