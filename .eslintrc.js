54dbdb2fe05beb63fae0aea00c15286d01a1a2e4
Chris McNeese <christophermcneese@mac.com>
2019-10-26 15:33:41 -0500

    update eslint to eslint4 addon

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,32 +4,10 @@ module.exports = {
     ecmaVersion: 2017,
     sourceType: 'module'
   },
-  extends: 'defaults',
+  extends: 'eslint:recommended',
   env: {
     browser: true
   },
   rules: {
   }
-  },
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js'
-      ],
-      parserOptions: {
-        sourceType: 'script',
-        ecmaVersion: 2015
-      },
-      env: {
-        browser: false,
-        node: true
-      }
-    }
-  ];
+};
