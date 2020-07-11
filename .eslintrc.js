01f4993d3bdf4ae7701cd8d8d690d0e184f1046b
Chris McNeese <christophermcneese@mac.com>
2020-07-11 13:13:03 -0500

    update ember version of project

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,6 +8,7 @@ module.exports = {
   env: {
     browser: true
   },
+<<<<<<< HEAD
   rules: {
   },
   overrides: {
@@ -28,6 +29,36 @@ module.exports = {
     env: {
       browser: false,
       node: true
+=======
+  rules: {},
+  overrides: [
+    // node files
+    {
+      files: [
+        '.eslintrc.js',
+        '.template-lintrc.js',
+        'ember-cli-build.js',
+        'testem.js',
+        'blueprints/*/index.js',
+        'config/**/*.js',
+        'lib/*/index.js',
+        'server/**/*.js'
+      ],
+      parserOptions: {
+        sourceType: 'script'
+      },
+      env: {
+        browser: false,
+        node: true
+      },
+      plugins: ['node'],
+      extends: ['plugin:node/recommended'],
+      rules: {
+        // this can be removed once the following is fixed
+        // https://github.com/mysticatea/eslint-plugin-node/issues/77
+        'node/no-unpublished-require': 'off'
+      }
+>>>>>>> e098042... message
     }
   }
 };
