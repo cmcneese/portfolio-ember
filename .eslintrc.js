879629905632b71c10d58b527ebbd02b87cd8dff
Chris McNeese <christophermcneese@mac.com>
2020-07-19 18:23:37 -0500

    edit eslintr

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,63 +2,31 @@ module.exports = {
   root: true,
   parserOptions: {
     ecmaVersion: 2017,
-    sourceType: 'module'
+    sourceType: "module"
   },
-  extends: 'eslint:recommended',
+  extends: "eslint:recommended",
   env: {
     browser: true
   },
-<<<<<<< HEAD
-  rules: {
-  },
+  rules: {},
   overrides: {
     // node files
     files: [
-      '.eslintrc.js',
-      '.template-lintrc.js',
-      'ember-cli-build.js',
-      'testem.js',
-      'blueprints/*/index.js',
-      'config/**/*.js',
-      'lib/*/index.js'
+      ".eslintrc.js",
+      ".template-lintrc.js",
+      "ember-cli-build.js",
+      "testem.js",
+      "blueprints/*/index.js",
+      "config/**/*.js",
+      "lib/*/index.js"
     ],
     parserOptions: {
-      sourceType: 'script',
+      sourceType: "script",
       ecmaVersion: 2017
     },
     env: {
       browser: false,
       node: true
-=======
-  rules: {},
-  overrides: [
-    // node files
-    {
-      files: [
-        '.eslintrc.js',
-        '.template-lintrc.js',
-        'ember-cli-build.js',
-        'testem.js',
-        'blueprints/*/index.js',
-        'config/**/*.js',
-        'lib/*/index.js',
-        'server/**/*.js'
-      ],
-      parserOptions: {
-        sourceType: 'script'
-      },
-      env: {
-        browser: false,
-        node: true
-      },
-      plugins: ['node'],
-      extends: ['plugin:node/recommended'],
-      rules: {
-        // this can be removed once the following is fixed
-        // https://github.com/mysticatea/eslint-plugin-node/issues/77
-        'node/no-unpublished-require': 'off'
-      }
->>>>>>> e098042... message
     }
   }
 };
